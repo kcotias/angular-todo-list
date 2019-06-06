fd655ae382f6d2089ce79df4cc8fa0cab92c4865
kcotias <kaianexperia@gmail.com>
2019-06-06 20:27:33 -0300

    add services

--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { TodoService } from './index';
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+  ],
+  providers: [
+    TodoService
   ]
+
 })
 export class TodosModule { }
